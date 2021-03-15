02ebc28a1a6532c5d382fa0ae7f8045eb0250b94
Michael Knopke <knopkem@gmail.com>
2021-03-15 15:49:00 +0100

    using ormconfig file instead of hard coding

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudiesModule } from './studies/studies.module';
 import { FilesModule } from './files/files.module';
-import { Study } from './studies/entities/study.entity';
-import { Series } from './studies/entities/series.entity';
-import { Image } from './studies/entities/image.entity';
-import { Patient } from './studies/entities/patient.entity';
 import { WadouriModule } from './wadouri/wadouri.module';
 import { WadorsModule } from './wadors/wadors.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { getConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'dicomweb',
-      entities: [Patient, Study, Series, Image],
-      synchronize: true,
-      logging: [],
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
     }),
     StudiesModule,
     FilesModule,
